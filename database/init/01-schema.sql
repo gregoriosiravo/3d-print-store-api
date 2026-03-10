c28b4a1de5cf4b3a1c3d89eb9e543fa3192c903d
Gregorio Siravo <gregoriosiravo@outlook.com>
2026-03-10 13:24:24 +0100

    modified users table

--- database/init/01-schema.sql
+++ database/init/01-schema.sql
@@ -5,6 +5,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
